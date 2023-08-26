de9b839c307a3f3f8caaaf6fb0be8166eb2ee3e4
Your Name <you@example.com>
2023-08-26 16:18:00 +0600

    rabiyul islam

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -7,11 +7,11 @@ const Navbar = () => {
         <div>
             <h1 className='mb-3 mt-5'>Update Profile</h1>
             <div>
-            <Link><a className={classes.text_1}  href="">Personal Info</a></Link>
-            <Link><a className={classes.text_1} href="">Educaton/Training</a></Link>
-            <Link><a className={classes.text_1} href="">Employment</a></Link>
-            <Link><a className={classes.text_1} href="">Other Information</a></Link>
-            <Link><a className={classes.text_1} href="">Photograph</a></Link>
+            <Link className='text-decoration-none fs-5'><a className={classes.text_1}  href="">Personal Info</a></Link>
+            <Link className='text-decoration-none'><a className={classes.text_1} href="">Educaton/Training</a></Link>
+            <Link className='text-decoration-none'><a className={classes.text_1} href="">Employment</a></Link>
+            <Link className='text-decoration-none'><a className={classes.text_1} href="">Other Information</a></Link>
+            <Link className='text-decoration-none'><a className={classes.text_1} href="">Photograph</a></Link>
             </div>
         </div>
     );
